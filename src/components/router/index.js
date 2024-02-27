3bcc608dbde06ce76879bbcaa294c9e407c844cb
David wanjohi <davidwagura83@gmail.com>
2024-02-27 12:20:31 +0300

    add index.js

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CreateView from '../views/CreateView.vue'
-import EditView from '../views/students/EditView.vue'
-import ShowView from '../views/ShowView.vue'
+import CreateView from '../views/members/CreateView.vue'
+// import EditView from '../views/students/EditView.vue'
+import ShowView from '../views/members/ShowView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CreateView.vue')
+      component: () => import('../views/members/CreateView.vue')
     },
     {
       path: '/edit/:id',
